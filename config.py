3b7073c1c44b5d8fcf3b3b2459c8372a3ea2af1e
nndat <nguyengocdat90@gmail.com>
2019-10-23 08:55:17 +0700

    restruct project

--- config.py
+++
@@ -1,9 +0,0 @@
-import os
-
-
-class Configuration():
-    APP_DIR = os.path.dirname(os.path.realpath(__file__))
-    SQLALCHEMY_DATABASE_URI = f'sqlite:///{APP_DIR}/blog.db'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = 'development'
-    DEBUG = True
